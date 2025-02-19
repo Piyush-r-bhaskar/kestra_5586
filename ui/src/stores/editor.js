80aa3e01090817c1be07ebee1e97155f8fa3c58d
Piyush-r-bhaskar <prbhasker0012@gmail.com>
2025-02-19 19:36:44 +0530

    chore(ui): Improvement to namespace editor empty state.

--- ui/src/stores/editor.js
+++ ui/src/stores/editor.js
@@ -7,6 +7,7 @@ export default {
         current: undefined,
         tabs: [],
         view: undefined,
+        treeData: [],
     },
     mutations: {
         updateOnboarding(state) {
@@ -95,6 +96,13 @@ export default {
         },
         changeView(state, view) {
             state.view = view;
+        },
+        refreshTree(state) {
+            state.explorerVisible = true;
+            state.treeRefresh = Date.now();
+        },
+        setTreeData(state, data) {
+            state.treeData = data;
         },
     },
 };
